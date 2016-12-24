0e5690d2dcb6579bc596c24df66b44cdf0ffa679
Danillo Pereira <pk.danillo@gmail.com>
2016-12-24 00:07:20 -0200

    Four of a Kind - Ranking

--- apps/core/lib/core/ranking.ex
+++ apps/core/lib/core/ranking.ex
@@ -1,6 +1,9 @@
 defmodule Core.Ranking do
   import Core.RankingGuards, only: [is_sequence: 5]
 
+  def get_value([{_a, _}, {b, _}, {b, _}, {b, _}, {b, _}]), do: 6
+  def get_value([{a, _}, {a, _}, {a, _}, {a, _}, {_b, _}]), do: 6 
+
   def get_value([{_,s}, {_,s}, {_,s}, {_,s}, {_,s}]), do: 5
 
   def get_value([{a, _}, {b, _}, {c, _}, {d, _}, {e, _}]) when is_sequence(a, b, c, d, e), do: 4
--- apps/core/test/core/ranking_test.exs
+++ apps/core/test/core/ranking_test.exs
@@ -3,6 +3,16 @@ defmodule Core.RankingTest do
   use Quixir
   alias Core.Ranking
 
+  test "this combination return 6 (Four of a Kind -> 8c, Ad, As, Ac, Ah)" do
+    hand = [{8, :clubs}, {8, :diamonds}, {8, :spades}, {8, :clubs}, {14, :hearts}]
+    assert Ranking.get_value(hand) == 6
+  end
+
+  test "this combination return 6 (Four of a Kind -> 8c, 8d, 8s, 8c, Ah)" do
+    hand = [{8, :clubs}, {14, :diamonds}, {14, :spades}, {14, :clubs}, {14, :hearts}]
+    assert Ranking.get_value(hand) == 6
+  end
+
   test "this combination return 5 (Flush -> 3c, 4c, 6c, 8c, Qc)" do
     hand = [{3, :clubs}, {4, :clubs}, {6, :clubs}, {8, :clubs}, {12, :clubs}]
     assert Ranking.get_value(hand) == 5
